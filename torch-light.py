6e05bc79df3108efde906c3349ab089c3719b5c7
Ritesh Yadav <daydreamingguy941@gmail.com>
2022-04-11 16:49:16 +0530

    Added Callbacks

--- torch-light.py
+++ torch-light.py
@@ -1,5 +1,6 @@
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning.callbacks import Callback
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -30,6 +31,39 @@ class CIFAR10DataModule(pl.LightningDataModule):
         # Assign test dataset for use in dataloader(s)
         if stage == 'test' or stage is None:
             self.cifar_test = CIFAR10(self.data_dir, train=False, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.cifar_val, batch_size=self.batch_size)
+
+    def test_dataloader(self):
+        return DataLoader(self.cifar_test, batch_size=self.batch_size)
+
+
+# Setting Up some Callbacks
+
+class ImagePredictionLogger(Callback):
+    def __init__(self, val_samples, num_samples=32):
+        super().__init__()
+        self.num_samples = num_samples
+        self.val_imgs, self.val_labels = val_samples
+
+    def on_validation_epoch_end(self, trainer, p1_module):
+        val_imgs = self.val_imgs.to(device=p1_module.device)
+        val_labels = self.val_labels.to(device=p1_module.device)
+                # Get model prediction
+        logits = pl_module(val_imgs)
+        preds = torch.argmax(logits, -1)
+        # Log the images as wandb Image
+        trainer.logger.experiment.log({
+            "examples":[wandb.Image(x, caption=f"Pred:{pred}, Label:{y}") 
+                           for x, pred, y in zip(val_imgs[:self.num_samples], 
+                                                 preds[:self.num_samples], 
+                                                 val_labels[:self.num_samples])]
+            })
+
 
 class LitModule(pl.LightningModule):
     def __init__(self, input_shape, num_classes, learning_rate=2e-4):
